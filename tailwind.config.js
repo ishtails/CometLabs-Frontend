33003afdab8adbb9305d9c2491540c20cfca3ec1
Styles <bcs_2021035@iiitm.ac.in>
2023-07-13 20:23:37 +0530

    app config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,19 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+    'node_modules/preline/dist/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    "fontFamily": {
+      "lexend": "Lexend",
+      sans: ["Inter", 'sans-serif']
+     },
+
+    extend: {
+       "boxShadow": {
+        "card-shadow": "0px 0px 0px 0px rgba(38,50,56,0.1), -1px 1px 3px 0px rgba(38,50,56,0.1), -2px 4px 5px 0px rgba(38,50,56,0.09), -5px 9px 6px 0px rgba(38,50,56,0.05), -9px 17px 8px 0px rgba(38,50,56,0.01), -15px 26px 8px 0px rgba(38,50,56,0)"
+       },
+    },
   },
-  plugins: [],
 }
