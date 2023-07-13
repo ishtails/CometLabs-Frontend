49a112cfa429be8e4294709ae4b011e0ec4c48ad
Styles <bcs_2021035@iiitm.ac.in>
2023-07-13 20:23:46 +0530

    Create NavBar.tsx

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import logo from '../assets/Weiss Logo.svg'
+import { AiOutlineMail } from "react-icons/ai";
+
+const NavBar = () => {
+  return (
+    <div className="flex mt-5 mx-5 justify-between max-w-6xl m-auto items-center">
+        <img src={logo} alt="logo" title="Weiss" className="w-32"/>
+
+        <div className="flex space-x-4 font-semibold text-slate-800">
+            <p>Home</p>
+            <p>Products</p>
+            <p>Contact Us</p>
+        </div>
+
+        <button className="flex items-center space-x-2 bg-slate-800 text-white p-2">
+            <p>Contact</p>
+            <AiOutlineMail />
+        </button>
+    </div>
+  )
+}
+
+export default NavBar
