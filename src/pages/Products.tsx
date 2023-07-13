7765c4fcda33ff8edf93a021c922a99a30a35eb8
Styles <bcs_2021035@iiitm.ac.in>
2023-07-13 22:56:20 +0530

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,25 +1,44 @@
 import image_1 from "../assets/Image_2.png";
 import image_2 from "../assets/Image_3.png";
 import image_3 from "../assets/Image_4.png";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import Card from "../components/Card";
+import image_5 from "../assets/Image_5.png";
+import Rectangle_Box from "../components/Rectangle_Box";
 
 const Products = () => {
   return (
-    <div className="bg-slate-100">
-      <div className="flex mt-14 items-center justify-between mx-5 lg:mx-auto max-w-6xl">
-        <div className="flex flex-col">
-          <img src={image_1} alt="" className="" />
-          <div className="flex flex-col">
-            <h1 className="font-lexend font-semibold">Title</h1>
-            <p className="">
-              Highlight the benefits of signing up for an appointment, online
-              class, or video consultation
-            </p>
-            <button className="flex items-center space-x-2">
-              <p>Contact</p>
-              <AiOutlineArrowRight />
-            </button>
+    <div className="bg-slate-100 h-[120vh]">
+      <div className="flex flex-col mt-14 justify-between pt-14">
+        <h1 className="font-lexend text-5xl font-semibold text-center mb-12">
+          Products
+        </h1>
+
+        <div className="relative flex items-center justify-between mx-auto  max-w-6xl">
+          <Card
+            src={image_1}
+            title={`VR Headset`}
+            body={`Verkehrskonzepte sind ein entscheidender Faktor jeglicher Infrastruktur. Das Ingenieurbüro Weiss hat deshalb ein Expertenteam aufgebaut.`}
+          />
+          <Card
+            src={image_2}
+            title={`Drones`}
+            body={`Verkehrskonzepte sind ein entscheidender Faktor jeglicher Infrastruktur. Das Ingenieurbüro Weiss hat deshalb ein Expertenteam aufgebaut.`}
+          />
+          <Card
+            src={image_3}
+            title={`Digital Cameras`}
+            body={`Verkehrskonzepte sind ein entscheidender Faktor jeglicher Infrastruktur. Das Ingenieurbüro Weiss hat deshalb ein Expertenteam aufgebaut.`}
+          />
+
+          <div className="absolute top-[120%]">
+            <Rectangle_Box />
           </div>
+
+          <img
+            src={image_5}
+            alt="earpods"
+            className="absolute top-[130%] right-0 w-[50%]"
+          />
         </div>
       </div>
     </div>
