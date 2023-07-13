be3a29fc03bde51914b764f2b0a1e3c68b8db0b7
Styles <bcs_2021035@iiitm.ac.in>
2023-07-13 20:23:48 +0530

    Create Hero.tsx

---
+++ src/pages/Hero.tsx
@@ -0,0 +1,20 @@
+import image_1 from '../assets/Image_1.png'
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+const Hero = () => {
+  return (
+    <div className="h-full flex mt-14 items-center justify-between mx-5 lg:mx-auto max-w-6xl">
+        <div className="w-[30%] space-y-4">
+            <h1 className="font-lexend font-bold text-4xl">Describe the value of booking an appointment</h1>
+            <p className="text-slate-500">No need to get clever. Tell people exactly what you're offering, then use this space to communicate your key value proposition.</p>
+            <button className="flex items-center space-x-2 bg-slate-800 text-white p-2">
+            <p>Get in touch</p>
+            <AiOutlineArrowRight />
+        </button>
+        </div>
+        <img src={image_1} alt="logo" title="Weiss" className="w-[50%]"/>
+    </div>
+  )
+}
+
+export default Hero
