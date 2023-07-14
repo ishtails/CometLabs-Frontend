25a0eb1b822d7da156e52fd01b25d50dbc6760e3
Styles <bcs_2021035@iiitm.ac.in>
2023-07-14 15:06:54 +0530

    added react scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ import Footer from "./pages/Footer";
 import Hero from "./pages/Hero";
 import Products from "./pages/Products";
 import Reviews from "./pages/Reviews";
+import { animateScroll as scroll } from "react-scroll";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="bg-white relative min-h-screen">
+    <div className="bg-white relative min-h-screen scroll-smooth">
       {/* Navbar */}
-      <NavBar />
+      <div className="sticky top-0 z-50 bg-white">
+        <NavBar />
+      </div>
 
       {/* Hero */}
       <Hero />
@@ -17,10 +39,22 @@ export default function App() {
       <Products />
 
       {/* Reviews */}
+
       <Reviews />
 
       {/* Footer */}
       <Footer />
+
+      {showButton && (
+        <button
+          className="fixed bottom-10 right-10"
+          onClick={() => {
+            scroll.scrollToTop();
+          }}
+        >
+          <BsFillArrowUpCircleFill style={{ fontSize: "1.5em" }} />
+        </button>
+      )}
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,30 @@
-import logo from '../assets/Weiss Logo.svg'
-import { AiOutlineMail } from "react-icons/ai";
+import logo from "../assets/Weiss Logo.svg";
+import { BiLogInCircle } from "react-icons/bi";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   return (
-    <div className="flex mt-5 mx-10 justify-between max-w-6xl xl:mx-auto items-center">
-        <img src={logo} alt="logo" title="Weiss" className="w-32"/>
+    <div className="flex py-5 px-10 justify-between max-w-6xl xl:mx-auto items-center">
+      <img src={logo} alt="logo" title="Weiss" className="w-32" />
 
-        <div className="hidden sm:flex space-x-4 font-semibold text-slate-800">
-            <p>Home</p>
-            <p>Products</p>
-            <p>Contact Us</p>
-        </div>
+      <div className="hidden sm:flex space-x-4 font-semibold text-slate-700">
+        <Link to="products" spy={true} smooth={true} duration={500}>
+          <p className="cursor-pointer">Products</p>
+        </Link>
+        <Link to="reviews" spy={true} smooth={true} offset={-10} duration={700}>
+          <p className="cursor-pointer">Reviews</p>
+        </Link>
+        <Link to="support" spy={true} smooth={true} offset={-10} duration={700}>
+          <p className="cursor-pointer">Support</p>
+        </Link>
+      </div>
 
-        <button className="flex items-center space-x-2 bg-slate-800 text-white p-2">
-            <p>Contact</p>
-            <AiOutlineMail />
-        </button>
+      <button className="flex items-center space-x-2 bg-slate-800 text-white p-2">
+        <p>Sign up</p>
+        <BiLogInCircle />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
