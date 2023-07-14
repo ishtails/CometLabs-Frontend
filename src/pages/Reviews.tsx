be2ccf51d5a2ce1f45157b75c578518d52260bd0
Styles <bcs_2021035@iiitm.ac.in>
2023-07-14 13:26:08 +0530

    Update Reviews.tsx

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -2,15 +2,15 @@ import Card from "../components/Card";
 
 const Reviews = () => {
   return (
-    <div className="min-h-screen mx-5 flex flex-col pt-80 max-w-6xl xl:mx-auto">
-      <div className="flex flex-col mb-24">
+    <div className="min-h-screen flex flex-col pt-80 max-w-6xl xl:mx-auto">
+      <div className="flex flex-col mb-16 items-center self-center">
         <h1 className="font-bold text-5xl">Reviews</h1>
-        <p className="text-slate-500">
+        <p className="text-slate-500 mt-4">
           Here is what our customers say about us
         </p>
       </div>
 
-      <div className="flex items-center justify-around">
+      <div className="flex items-center justify-around mr-8">
         <Card
           title="Customer 1"
           body="Pellentesque mauris arcu nec gravida amet nec. Duis senectus cras semper cursus bibendum phasellus vehicula porttitor..."
